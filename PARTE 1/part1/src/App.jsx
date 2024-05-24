345a11b2494f44f6b3b7ec6e2058cd9744ca2fe2
andretokumoto <andretokumoto@gmail.com>
2024-05-24 15:45:13 -0300

    Multiplos componentes

--- PARTE 1/part1/src/App.jsx
+++ PARTE 1/part1/src/App.jsx
@@ -1,15 +1,18 @@
-const App = () => {
-  const hoje = new Date()
-  const a = 10
-  const b = 20
-  console.log(hoje, a+b)
-
+const Hello = () => {
   return (
     <div>
-      <p>Olá, mundo! Hoje é {hoje.toString()}</p>
-      <p>
-        {a} mais {b} é {a + b}
-      </p>
+      <p>Olá, mundo!</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Olá a todos!</h1>
+
+      <Hello />
+      <Hello />
     </div>
   )
 }
