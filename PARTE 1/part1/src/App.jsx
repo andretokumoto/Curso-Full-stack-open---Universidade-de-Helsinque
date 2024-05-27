1e14ac28f50ff0a2ff73d29b055d3a4cca3c43df
andretokumoto <andretokumoto@gmail.com>
2024-05-27 13:16:36 -0300

    1.7: unicafe — 2º passo

--- PARTE 1/part1/src/App.jsx
+++ PARTE 1/part1/src/App.jsx
@@ -1,10 +1,60 @@
 import { useState } from 'react'
 
+
+
+
+
+
 const App = () => {
-  // salve os cliques de cada botão em seu próprio estado
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [ponderada,setPonderada]=useState(0)
+
+
+  const clique = (feedback) =>{
+
+      switch(feedback){
+
+        case 'good': {
+          setGood(good+1)
+          setPonderada(ponderada+1)
+          break
+        }
+
+        case 'neutral':{
+          setNeutral(neutral+1)
+          break
+        }
+        case 'bad':{
+          setBad(bad+1)
+          setPonderada(ponderada-1)
+          break
+        }
+        default:break
+      }
+
+  }
+
+  const Average = (props)=>{
+    return(
+      <p>
+         pontuação média: {props.ponderada/props.total}
+      </p>  
+    ) 
+}
+
+
+const Total_positivo = (props)=>{
+  const total = props.total
+  const good = props.good
+  return(
+    <p>
+       Porcentagem Positiva: {(good/total)*100}%
+    </p>  
+  ) 
+}
 
   return (
     <div>
@@ -14,15 +64,15 @@ const App = () => {
       </b>
 
      <p>
-      <button onClick={() => setGood(good+1)}>
+      <button onClick={() => clique('good')}>
             Good   
       </button>
 
-      <button onClick={() => setNeutral(neutral+1)}>
+      <button onClick={() => clique('neutral')}>
             Neutral   
       </button>
 
-      <button onClick={() => setBad(bad+1)}>
+      <button onClick={() => clique('bad')}>
             BAD   
       </button>
 
@@ -45,7 +95,15 @@ const App = () => {
      </p>
        
 
+     <p>
+        TOTAL: {good+bad+neutral}
+     </p>
+
+     <Average ponderada={ponderada} total = {good+bad+neutral} />
+     <Total_positivo good={good} total ={good+bad+neutral}/>
+
     </div>
+
   )
 }
 
