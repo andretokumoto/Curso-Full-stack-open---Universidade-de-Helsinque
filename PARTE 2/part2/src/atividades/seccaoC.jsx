ff8e73c209e7e09bad1d2f3459b2790e7bbc6ba0
andretokumoto <andretokumoto@gmail.com>
2024-06-20 15:30:36 -0300

    aula c

--- PARTE 2/part2/src/atividades/seccaoC.jsx
+++ PARTE 2/part2/src/atividades/seccaoC.jsx
@@ -1,6 +1,26 @@
-const SeccaoC = ()=>{
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './Note.jsx'
+
+const SeccaoC = () => {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
 
+  useEffect(() => {
+    console.log('effect (efeito)')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled (promessa resolvida)')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render (renderiza)', notes.length, 'notes (notas)')
+
+  // ...
 }
+
 
 export default SeccaoC 
