05b9084a1d482069ef923111f7f572efe9122292
andretokumoto <andretokumoto@gmail.com>
2024-08-13 14:27:40 -0300

    biblioteca moment

--- PARTE 3/index.js
+++ PARTE 3/index.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 const express = require('express')
 const app = express()
 
@@ -107,6 +108,16 @@ app.post('/api/notes', (request, response) => {
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+
+app.get('/api/info',(request, response) => {
+
+    const qtd_dados = persons.length
+    const data_hoje = moment().format('YYYY-MM-DD');
+    console.log('qtd:',qtd_dados)
+
+
+})
+
 
 //-------------------------------------------------------------
   const PORT = 3001
