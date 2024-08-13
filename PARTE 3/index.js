d1e60eea7b45e8ba5a679160b0d0f84e28059b42
andretokumoto <andretokumoto@gmail.com>
2024-08-13 14:41:18 -0300

    3.3: Phonebook backend — 3º passo

--- PARTE 3/index.js
+++ PARTE 3/index.js
@@ -117,6 +117,17 @@ app.get('/api/info',(request, response) => {
 
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(persons => persons.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 
 //-------------------------------------------------------------
   const PORT = 3001
