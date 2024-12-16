f46211a8b2a73108f80873d5fc7c9497057e9ac5
andretokumoto <andretokumoto@gmail.com>
2024-12-16 16:23:53 -0300

    criação da parte 4 do curso

---
+++ PARTE 4/index.js
@@ -0,0 +1,181 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const app = express()
+const errorHandler = require('./errorHandler');
+
+app.use(morgan('tiny'))
+app.use(express.static('build'))
+app.use(express.json())
+
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = 'mongodb+srv://andretokumoto:'+{password}+'@fullstackopen.vbjd3.mongodb.net/?retryWrites=true&w=majority&appName=FullStackOpen';
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+module.exports = Person;
+
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+  
+//-----------notes------------------------------------------  
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+  
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+//------------persons-----------------------------------------
+
+//retorna a lista telefonica
+/*app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})*/
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
+});
+
+//retorna informações da lista
+app.get('/api/info',(request, response) => {
+    const qtd_dados = persons.length
+    const data_hoje = new Date();
+    
+    response.send(`<h1>pessoas na lista: ${qtd_dados}</h1><p>Data: ${data_hoje}</p>`);
+})
+
+//pega uma pessoa especifica da lista
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(persons => persons.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+//deletar um elemento da lista
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).json({ error: 'person not found' });
+      }
+    })
+    .catch(error => next(error)); // Passe o erro para o middleware de erros
+});
+
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing',
+    });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson);
+    })
+    .catch(error => next(error)); // Passe o erro para o middleware de erros
+});
+
+
+app.use(errorHandler);
+
+//-------------------------------------------------------------
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+//--------------------
